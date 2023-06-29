6aaadbc16e8ebb947e1604d4353e57e44813d108
nickberezny <nickberezny@hotmail.com>
2023-06-29 12:32:35 -0400

    now c++

---
+++ test_socket.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <net/if.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+
+#include <linux/can.h>
+#include <linux/can/raw.h>
+
+#include "Definitions.h"
+
+int main(int argc, char** argv)
+{
+
+	int s;
+	struct sockaddr_can addr;
+	struct ifreq ifr;
+
+	s = socket(PF_CAN, SOCK_RAW, CAN_RAW);
+
+	strcpy(ifr.ifr_name, "can0" );
+	ioctl(s, SIOCGIFINDEX, &ifr);
+
+	addr.can_family = AF_CAN;
+	addr.can_ifindex = ifr.ifr_ifindex;
+
+	bind(s, (struct sockaddr *)&addr, sizeof(addr));
+
+}
