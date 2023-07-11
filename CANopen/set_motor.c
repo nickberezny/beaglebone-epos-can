804cb0c3dff3e4c0e21417e41ce1bb3d8da10ebc
nickberezny <nickberezny@hotmail.com>
2023-07-11 14:42:33 -0400

    removed some sdo's

--- CANopen/set_motor.c
+++ CANopen/set_motor.c
@@ -7,8 +7,8 @@
 
 void set_motor(int fd, double set_val)
 {
-
-	PDO_send(fd, PDO_TX1_ID+MOTOR_EPOS_R_ID, 2, set_val);
+	printf("Set torque: %d\n", (int)set_val);
+	PDO_send(fd, PDO_TX1_ID+MOTOR_EPOS_R_ID, 2, (int)set_val);
 
 	return;
 }
