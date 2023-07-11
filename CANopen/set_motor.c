5be0ec335d75f797edca754567c10938fe1cfde0
nickberezny <nickberezny@hotmail.com>
2023-07-11 15:08:44 -0400

    edits

--- CANopen/set_motor.c
+++ CANopen/set_motor.c
@@ -9,7 +9,7 @@
 void set_motor(int fd, double set_val)
 {
 	printf("Set torque: %d\n", (int)set_val);
-	PDO_send(fd, PDO_TX1_ID+MOTOR_EPOS_R_ID, 2, (int)set_val);
+	//PDO_send(fd, PDO_TX1_ID+MOTOR_EPOS_R_ID, 2, (int)set_val);
 
 	return;
 }
