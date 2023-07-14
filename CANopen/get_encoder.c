8e3b2345bd69012bd13e9bda1be9862c5efccb81
nickberezny <nickberezny@hotmail.com>
2023-07-14 16:04:09 -0400

    get encoder now check fors f.id

--- CANopen/get_encoder.c
+++ CANopen/get_encoder.c
@@ -18,9 +18,14 @@ double get_encoder(int fd)
 	int ret = PDO_read(fd, &f, timeout);
 	printf("PDO ret: %d\n", ret);
 
-	int32_t pos = ((uint32_t)f.data[0]<<0) | ((uint32_t)f.data[1]<<8) | ((uint32_t)f.data[2]<<16) | ((uint32_t)f.data[3]<<24);
-	int32_t vel = ((uint32_t)f.data[4]<<0) | ((uint32_t)f.data[5]<<8) | ((uint32_t)f.data[6]<<16) | ((uint32_t)f.data[7]<<24);
+	int32_t pos = 0;
 
-	printf("Position: %d\n", pos);
+	if(f.id == PDO_TX2_ID + MOTOR_EPOS_R_ID)
+	{
+		int32_t pos = ((uint32_t)f.data[0]<<0) | ((uint32_t)f.data[1]<<8) | ((uint32_t)f.data[2]<<16) | ((uint32_t)f.data[3]<<24);
+		int32_t vel = ((uint32_t)f.data[4]<<0) | ((uint32_t)f.data[5]<<8) | ((uint32_t)f.data[6]<<16) | ((uint32_t)f.data[7]<<24);
+		printf("Position: %d\n", pos);
+	}
+	
 	return (double)pos;
 }
