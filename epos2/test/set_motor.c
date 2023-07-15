da066f66258a80343e42f52663e18a93fb65f3f1
nickberezny <nickberezny@hotmail.com>
2023-07-15 15:04:31 -0400

    switched epos2 to torque mode

--- epos2/test/set_motor.c
+++ epos2/test/set_motor.c
@@ -29,7 +29,7 @@ void set_motor(int fd, double set_val)
 	printf("Set torque: %d\n", (int)set_val);
 	//PDO_send(fd, PDO_TX1_ID+MOTOR_EPOS_R_ID, 2, (int)set_val);
 	//vel_set_speed_right((int)set_val);
-	set_torque((int)set_val, MOTOR_EPOS_R_ID);
+	printf("pdo err: %d\n", set_torque((int)set_val, MOTOR_EPOS_R_ID));
 	return;
 }
 
