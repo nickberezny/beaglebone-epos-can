e083198acea471381c5de60aae921f99ec171c32
nickberezny <nickberezny@hotmail.com>
2023-07-19 15:56:19 -0400

    edits

--- CANopen/motor/commands.c
+++ CANopen/motor/commands.c
@@ -3,7 +3,7 @@
 #include "epos.h"
 #include "../canopen/PDO.h"
 
-#include<stdio.h>
+#include <stdio.h>
 #include <inttypes.h>
 #include <math.h>
 
@@ -55,13 +55,13 @@ int sort_read(int32_t* pos, my_can_frame f, int * num_of_reads)
 			enc = ((uint32_t)f.data[0]<<0) | ((uint32_t)f.data[1]<<8) | ((uint32_t)f.data[2]<<16) | ((uint32_t)f.data[3]<<24);
 			pos[0] = enc;//motor_enc_to_mm(enc);
 			*num_of_reads = *num_of_reads + 1;
-			printf("Node 1: %f\n" enc);
+			printf("Node 1: %f\n", enc);
 			break;
 		case(PDO_TX2_ID + 2):
 			enc = ((uint32_t)f.data[0]<<0) | ((uint32_t)f.data[1]<<8) | ((uint32_t)f.data[2]<<16) | ((uint32_t)f.data[3]<<24);
 			pos[1] = enc;//motor_enc_to_mm(-enc);
 			*num_of_reads = *num_of_reads + 1;
-			printf("Node 2: %f\n" enc);
+			printf("Node 2: %f\n", enc);
 			break;
 		default:
 			break;
